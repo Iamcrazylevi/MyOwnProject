f305f14dea78fce18a797441a16d1b418ff6ffbe
Levi Meslan <534143@student.glu.nl>
2021-06-24 11:32:45 +0200

    Added Firebase

--- todoist/src/firebase.js
+++ todoist/src/firebase.js
@@ -1,13 +1,14 @@
-
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messageSenderId: '',
-    apiId: '',
+    apiKey: "AIzaSyCJoxvjYohMZJprFIUoYZKr3xaPGmKfgDc",
+    authDomain: "todoist-9b3f9.firebaseapp.com",
+    databaseURL: "https://todoist-9b3f9-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "todoist-9b3f9",
+    storageBucket: "todoist-9b3f9.appspot.com",
+    messagingSenderId: "469529045052",
+    appId: "1:469529045052:web:9494cbb541cd4471f83b39"
 });
 
 export { firebaseConfig as firebase };
